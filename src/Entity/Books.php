49719870763747e0c8eccba23dd1ff0ee6421680
hanane <nam.hana@yahoo.fr>
2019-02-05 11:41:14 +0100

    create entities

---
+++ src/Entity/Books.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BooksRepository")
+ */
+class Books
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $resume;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $edition;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $borrowDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $returnDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $userHistorical = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getEdition(): ?\DateTimeInterface
+    {
+        return $this->edition;
+    }
+
+    public function setEdition(\DateTimeInterface $edition): self
+    {
+        $this->edition = $edition;
+
+        return $this;
+    }
+
+    public function getBorrowDate(): ?\DateTimeInterface
+    {
+        return $this->borrowDate;
+    }
+
+    public function setBorrowDate(?\DateTimeInterface $borrowDate): self
+    {
+        $this->borrowDate = $borrowDate;
+
+        return $this;
+    }
+
+    public function getReturnDate(): ?\DateTimeInterface
+    {
+        return $this->returnDate;
+    }
+
+    public function setReturnDate(?\DateTimeInterface $returnDate): self
+    {
+        $this->returnDate = $returnDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUserHistorical(): ?array
+    {
+        return $this->userHistorical;
+    }
+
+    public function setUserHistorical(?array $userHistorical): self
+    {
+        $this->userHistorical = $userHistorical;
+
+        return $this;
+    }
+}
