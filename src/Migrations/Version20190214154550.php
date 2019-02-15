1ec29afb0c2a68b7b668f384b1c2547f267b437c
hanane <nam.hana@yahoo.fr>
2019-02-15 11:40:22 +0100

    essai de corriger la connexion

--- src/Migrations/Version20190214154550.php
+++ src/Migrations/Version20190214154550.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190214152125 extends AbstractMigration
+final class Version20190214154550 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,6 @@ final class Version20190214152125 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE users ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', ADD password VARCHAR(255) DEFAULT NULL, ADD email VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE books DROP borrower');
     }
 
@@ -32,6 +31,5 @@ final class Version20190214152125 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE books ADD borrower VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
-        $this->addSql('ALTER TABLE users DROP roles, DROP password, DROP email');
     }
 }
